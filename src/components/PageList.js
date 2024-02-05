5d3ecc1cd34b6b9e6c514f8ee60c6313674c3c73
Rajat Sharma <rajjusharmarajat@gmail.com>
2024-02-05 15:34:26 +0530

    pagelist created

--- src/components/PageList.js
+++ src/components/PageList.js
@@ -1,21 +1,25 @@
+import pageList from "./dummyData/dummyData";
+
 const PageList = () => {
   return (
     <>
       <div className="container-flex vh-100 bg-light py-5 px-5">
-        <div className="card py-1">
-          <h3 className="text-center text-primary">Page Title</h3>
-          <div className="card-body">
-            <p className="text-center text-secondary">Page Title</p>
-            <div className="d-flex flex-row-reverse">
-              <button className="btn btn-outline-primary text-secondary mx-1    ">
-                delete
-              </button>
-              <button className="btn btn-outline-primary text-secondary mx-1">
-                edit
-              </button>
+        {pageList.map((page) => (
+          <div className="card py-1 my-1">
+            <h3 className="text-center text-primary">{page.title}</h3>
+            <div className="card-body">
+              <p className="text-center text-secondary">{page.comment}</p>
+              <div className="d-flex flex-row-reverse">
+                <button className="btn btn-outline-primary text-secondary mx-1    ">
+                  delete
+                </button>
+                <button className="btn btn-outline-primary text-secondary mx-1">
+                  edit
+                </button>
+              </div>
             </div>
           </div>
-        </div>
+        ))}
       </div>
     </>
   );
---
+++ src/components/dummyData/dummyData.js
@@ -0,0 +1,32 @@
+const pageList = [
+  {
+    title: "page1",
+    comment: "Navbar added",
+  },
+
+  {
+    title: "page2",
+    comment: "bug fixed",
+  },
+  {
+    title: "page3",
+    comment: "backend created",
+  },
+
+  {
+    title: "page4",
+    comment: "api tested",
+  },
+
+  {
+    title: "page5",
+    comment: "frontend created",
+  },
+
+  {
+    title: "page6",
+    comment: "UI tested",
+  },
+];
+
+export default pageList;
